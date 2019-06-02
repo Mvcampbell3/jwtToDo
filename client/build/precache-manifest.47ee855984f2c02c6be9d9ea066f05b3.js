91422d63abaa78325106fb6a2a6022300ea020e7
Mvcampbell3 <mvcampbell3@gmail.com>
2019-06-02 15:00:23 -0400

    ran npm run-script build

---
+++ client/build/precache-manifest.47ee855984f2c02c6be9d9ea066f05b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e20eebf08e4c65fe8e2e",
+    "url": "/static/js/main.a6684925.chunk.js"
+  },
+  {
+    "revision": "bef7ce99eeae994974da",
+    "url": "/static/js/2.ffae726f.chunk.js"
+  },
+  {
+    "revision": "e20eebf08e4c65fe8e2e",
+    "url": "/static/css/main.3db4ce70.chunk.css"
+  },
+  {
+    "revision": "5e4d3458352453532c4c59aad064b0d3",
+    "url": "/index.html"
+  }
+];
